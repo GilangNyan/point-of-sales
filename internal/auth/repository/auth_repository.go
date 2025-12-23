60748a65307f653e916ef91555f1134872b453f2
GilangNyan <sagilang@gmail.com>
2025-12-23 16:36:12 +0700

    fix: remove all codes for rewrite

--- internal/auth/repository/auth_repository.go
+++
@@ -1,7 +0,0 @@
-package repository
-
-type AuthRepository interface {
-	SetUserSession(userID int, sessionToken string) error
-	GetUserSession(userID int) (string, error)
-	DeleteUserSession(userID int) error
-}
--- internal/user_role/model/user_role_dto.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type AssignRolesDto struct {
-	UserID  string   `json:"userId" binding:"required,uuid"`
-	RoleIDs []string `json:"roleIds" binding:"required,uuid"`
-}
